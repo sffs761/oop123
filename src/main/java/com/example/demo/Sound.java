ef7fb1c34850702833e7b44884de3eaf366b4761
sffs761 <cuong02ht@gmail.com>
2021-12-23 19:03:30 +0700

    updated2

--- src/main/java/com/example/demo/Sound.java
+++ src/main/java/com/example/demo/Sound.java
@@ -1,79 +1,42 @@
-//package com.example.demo.Sound;
-//
-//import javax.sound.sampled.*;
-//import java.io.*;
-//import java.util.*;
-//
-//public class Sound {
-//    /**
-//     * Constructor.
-//     */
-//
-//    Sound() {}
-//
-//    /**
-//     * Play.
-//     */
-//
-//    public void Play(String name) throws UnsupportedAudioFileException,
-//            IOException, LineUnavailableException {
-//        String path =  "path"; // đường dẫn file wav
-//        File file = new File(path);
-//        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-//        Clip clip = AudioSystem.getClip();
-//        clip.open(audioStream);
-//        clip.start();
-//    }
-//
-//    /**
-//     * Loop.
-//     */
-//
-//    public void Loop(String name) throws UnsupportedAudioFileException,
-//            IOException, LineUnavailableException {
-//        String path = "path"; // đường dẫn file wav
-//        File file = new File(path);
-//        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-//        Clip clip = AudioSystem.getClip();
-//        clip.open(audioStream);
-//        clip.loop(Clip.LOOP_CONTINUOUSLY);
-//    }
-//
-//    /**
-//     * demoSound
-//     */
-//    public static void main (String[] args) throws UnsupportedAudioFileException,
-//            IOException, LineUnavailableException {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        File file = new File("01_Title Screen.wav");
-//        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-//        Clip clip = AudioSystem.getClip();
-//        clip.open(audioStream);
-//
-//        String response = "";
-//        while(!response.equals("Q")) {
-//            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
-//            System.out.print("Enter your choice: ");
-//
-//            response = scanner.next();
-//            response = response.toUpperCase();
-//
-//            switch(response) {
-//                case ("P"):
-//                    clip.start();
-//                    break;
-//                case ("S"): clip.stop();
-//                    break;
-//                case ("R"): clip.setMicrosecondPosition(0);
-//                    break;
-//                case ("Q"): clip.close();
-//                    break;
-//                default: System.out.println("Not a valid response");
-//            }
-//        }
-//        System.out.println("Bye!");
-//    }
-//}
+package com.example.demo;
+
+import javax.sound.sampled.*;
+import java.io.*;
+
+public class Sound {
+    /**
+     * Constructor.
+     */
+
+    Sound() {}
+
+    /**
+     * Play.
+     */
+
+    public void Play(String name) throws UnsupportedAudioFileException,
+            IOException, LineUnavailableException {
+        String path =  "E:\\Repo\\demo\\src\\main\\resources\\com\\example\\Sound\\" + name + ".wav";
+        File file = new File(path);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        clip.start();
+    }
+
+    /**
+     * Loop.
+     */
+
+    public void Loop(String name) throws UnsupportedAudioFileException,
+            IOException, LineUnavailableException {
+        String path = "E:\\Repo\\demo\\src\\main\\resources\\com\\example\\Sound\\" + name + ".wav";
+        File file = new File(path);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+}
 
